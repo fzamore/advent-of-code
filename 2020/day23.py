e1fd7a11d8045cfc5f9dfae6e6a4c56b0b5f57a1
Eric Zamore <fzamore@gmail.com>
2022-01-25 08:39:33 -0500

    using a singly- instead of a doubly-linked list for day23. thanks, reddit!

--- 2020/day23.py
+++ 2020/day23.py
@@ -25,15 +25,13 @@ def step(cups, curi):
 
 def step2(cups, cur):
   cupsLen = len(cups) - 1
-  curP, curN = cups[cur]
+  curN = cups[cur]
 
   # three cups to remove
   c1 = curN
-  _, c2 = cups[c1]
-  _, c3 = cups[c2]
-  _, c4 = cups[c3]
-  cups[cur] = (curP, c4)
-  cups[c4] = (cur, cups[c4][1])
+  c2 = cups[c1]
+  c3 = cups[c2]
+  cups[cur] = cups[c3]
 
   # subtract 1 (1-indexed) from current
   dest = ((cur - 2) % cupsLen) + 1
@@ -41,15 +39,12 @@ def step2(cups, cur):
     dest = ((dest - 2) % cupsLen) + 1
 
   # insert three cups after destination cup
-  destP, destN = cups[dest]
-  _, destNN = cups[destN]
-  cups[dest] = (destP, c1)
-  cups[destN] = (c3, destNN)
-  cups[c1] = (dest, c2)
-  cups[c3] = (c2, destN)
+  destN = cups[dest]
+  cups[dest] = c1
+  cups[c3] = destN
 
   # new current cup is next after current cup
-  return cups[cur][1]
+  return cups[cur]
 
 def part1():
   cups = [int(x) for x in readfile('day23.txt')[0]]
@@ -69,26 +64,23 @@ def part1():
 
 def part2():
   # `cups` is a 1-indexed array where each index stores a tuple with the 
-  # (next, prev) cup for that label, in clockwise order. Essentially, 
-  # a linked list stored in an array indexed by cup label.
-  # cups[0] is always None
+  # next cup for that label, in clockwise order. Essentially, 
+  # a singly-linked list stored in an array indexed by cup label.
+  # cups[0] is always `None`
 
   values = [int(x) for x in readfile('day23.txt')[0]]
   maxCup = 1000000
   # add one so we can maintain 1-indexing
   cups = [None] * (maxCup + 1)
   l = len(values)
-  for i in range(1, l - 1):
-    cups[values[i]] = (values[i - 1], values[i + 1])
+  for i in range(0, l - 1):
+    cups[values[i]] = values[i + 1]
   
   # add extra cups
-  cups[values[l - 1]] = (values[l - 2], max(values) + 1)
-  cups[max(values) + 1] = (values[l - 1], max(values) + 2)
-  for i in range(max(values) + 2, maxCup):
-    cups[i] = (i - 1, i + 1)
-
-  cups[values[0]] = (maxCup, values[1])
-  cups[maxCup] = (maxCup - 1, values[0])
+  cups[values[l - 1]] = max(values) + 1
+  for i in range(max(values) + 1, maxCup):
+    cups[i] = i + 1
+  cups[maxCup] = values[0]
 
   cur = values[0]
   
@@ -96,8 +88,8 @@ def part2():
   for i in range(0, n):
     cur = step2(cups, cur)
   
-  oneN = cups[1][1]
-  oneNN = cups[oneN][1]
+  oneN = cups[1]
+  oneNN = cups[oneN]
   print(oneN, oneNN, oneN * oneNN)
 
 part2()
