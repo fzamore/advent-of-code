34dcc2b347a9d0527995a747ea323a7989eb7ecd
Eric Zamore <fzamore@gmail.com>
2022-01-26 22:42:39 -0500

    day25 improvement. good lord what I was doing was dumb

--- 2020/day25.py
+++ 2020/day25.py
@@ -17,12 +17,24 @@ def transform2(n, s=7):
     s = (s * s) % m
   return result
 '''
-  
+
+# Obvious approach I copied from reddit. How I did not think of this is beyond me.
+def findLoopSize2(pub):
+  v = 1
+  n = 0
+  while v != pub:
+    v = (7 * v) % 20201227
+    n += 1
+  return n
+
+# Good lord, what the heck was I doing? This is silly and slow.
+'''
 def findLoopSize(pub):
   for i in range(1, 100000000):
     if transform(i) == pub:
       return i
   assert False, 'did not find loop size for public key: %d' % pub
+'''
 
 def part1():
   lines = readfile('day25.txt')
@@ -30,9 +42,9 @@ def part1():
   pub2 = int(lines[1])
   print('public keys', pub1, pub2)
 
-  loop1 = findLoopSize(pub1)
+  loop1 = findLoopSize2(pub1)
   print('loop size 1', loop1)
-  loop2 = findLoopSize(pub2)
+  loop2 = findLoopSize2(pub2)
   print('loop size 2', loop2)
   
   enc1 = transform(loop2, pub1)
