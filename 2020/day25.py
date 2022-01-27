b65d6b9c29bb76993f107dbc8fda9926a5e3244e
Eric Zamore <fzamore@gmail.com>
2022-01-26 22:06:01 -0500

    Day 25 done. AoC 2020 done

---
+++ 2020/day25.py
@@ -0,0 +1,43 @@
+from common.io import readfile
+
+def transform(n, s=7):
+  # Python has fast modular exponentiation by default
+  return pow(s, n, 20201227)
+  
+# Squaring exponentiation algorithm copied from 
+# https://en.wikipedia.org/wiki/Modular_exponentiation
+'''
+def transform2(n, s=7):
+  m = 20201227
+  result = 1
+  while n > 0:
+    if n % 2 == 1:
+      result = (result * s) % m
+    n = n >> 1
+    s = (s * s) % m
+  return result
+'''
+  
+def findLoopSize(pub):
+  for i in range(1, 100000000):
+    if transform(i) == pub:
+      return i
+  assert False, 'did not find loop size for public key: %d' % pub
+
+def part1():
+  lines = readfile('day25.txt')
+  pub1 = int(lines[0])
+  pub2 = int(lines[1])
+  print('public keys', pub1, pub2)
+
+  loop1 = findLoopSize(pub1)
+  print('loop size 1', loop1)
+  loop2 = findLoopSize(pub2)
+  print('loop size 2', loop2)
+  
+  enc1 = transform(loop2, pub1)
+  enc2 = transform(loop1, pub2)
+  assert enc1 == enc2, 'bad encryption keys: %d, %d' % (enc1, enc2)
+  print(enc1)
+
+part1()
