a9101167f33c37e704c2ac817bdd6e04f7176ec3
Eric Zamore <fzamore@gmail.com>
2022-04-08 13:59:59 -0400

    adding typehints so sparsegrid.py conforms to "mypy --strict"

--- common/sparsegrid.py
+++ common/sparsegrid.py
@@ -1,4 +1,16 @@
-def _nDimRange(d, minValues, maxValues):
+from typing import Any, cast, Iterable, Iterator, Optional, Sized
+
+# Type for an individual coordinate value. We only support integer coordinates.
+Coord = int
+
+# Type for a single set of coordinates (e.g., (-2, 3, 6)).
+Coords = tuple[Coord, ...]
+
+def _nDimRange(
+    d: int,
+    minValues: list[Coord],
+    maxValues: list[Coord],
+) -> Iterator[Coords]:
     if d == 0:
         yield ()
         return
@@ -7,46 +19,53 @@ def _nDimRange(d, minValues, maxValues):
         for i in range(minValues[d - 1], maxValues[d - 1] + 1):
             yield coords + (i,)
 
+# Sentinel value to indicate the cell has been marked.
+_MARKED = '_MARKED'
+
 class SparseGrid:
-    _MARKED = '_MARKED'
+    # Dimension of the grid.
+    _dimension: int
 
-    def __init__(self, dimension=2):
-        # Dimension of the grid
+    # Underlying value storage.
+    _map: dict[Coords, Any]
+
+    # Underlying storage for marking cells.
+    _marked: dict[Coords, dict[str, bool]]
+
+    # Arrays that store the minimum and maximum coordinate values in each dimension.
+    _minCoords: list[Coord]
+    _maxCoords: list[Coord]
+
+    def __init__(self, dimension: int = 2) -> None:
         self._dimension = dimension
-
-        # Underlying value storage
         self._map = {}
-
-        # Underlying storage for marking cells
         self._marked = {}
+        self._minCoords = []
+        self._maxCoords = []
 
-        # Arrays that store the minimum and maximum coordinate values in each dimension.
-        self._minCoords = [None] * dimension
-        self._maxCoords = [None] * dimension
-
-    def setValue(self, coords, value):
+    def setValue(self, coords: Coords, value: Any) -> None:
         self._validateCoords(coords)
         self._updateBoundaryCoords(coords)
         self._map[coords] = value
 
-    def getValue(self, coords, default=None):
+    def getValue(self, coords: Coords, default: Any = None) -> Any:
         self._validateCoords(coords)
         if self.hasValue(coords):
             return self._map[coords]
         return default
 
-    def hasValue(self, coords):
+    def hasValue(self, coords: Coords) -> bool:
         self._validateCoords(coords)
         return coords in self._map
 
-    def deleteValue(self, coords):
+    def deleteValue(self, coords: Coords) -> None:
         self._validateCoords(coords)
 
         if not self.hasValue(coords):
             return
         del self._map[coords]
 
-    def getAdjacentCoords(self, coords):
+    def getAdjacentCoords(self, coords: Coords) -> Iterator[Coords]:
         deltas = _nDimRange(
             self._dimension,
             [-1] * self._dimension,
@@ -57,13 +76,16 @@ class SparseGrid:
             if all(x == 0 for x in delta):
                 # Skip the zero delta.
                 continue
-            adjCoords = ()
+            adjCoords: Coords = ()
             # Apply the delta and concatente resulting coordinate in each dimension.
             for d in range(0, self._dimension):
                 adjCoords += ((coords[d] + delta[d]),)
             yield adjCoords
 
-    def getAdjacentCoordsInGrid(self, coords):
+    def getAdjacentCoordsInGrid(self, coords: Coords) -> Iterator[Coords]:
+        assert len(self._map) > 0, 'Cannot get adjacent coords of empty grid'
+        self._assertBoundaryCoords()
+
         for adjCoords in self.getAdjacentCoords(coords):
             notInGrid = False
             for d in range(self._dimension):
@@ -74,23 +96,27 @@ class SparseGrid:
                 continue
             yield adjCoords
 
-    def getAllCoordsInOrder(self, minCoords=None, maxCoords=None):
-        if not minCoords:
-            minCoords = self._minCoords
-        if not maxCoords:
-            maxCoords = self._maxCoords
-        return _nDimRange(self._dimension, tuple(minCoords), tuple(maxCoords))
+    def getAllCoordsInOrder(
+        self,
+        minCoords: Optional[list[Coord]] = None,
+        maxCoords: Optional[list[Coord]] = None,
+    ) -> Iterator[Coords]:
+        return _nDimRange(
+            self._dimension,
+            minCoords if minCoords is not None else self._minCoords,
+            maxCoords if maxCoords is not None else self._maxCoords,
+        )
 
-    def getAllCoords(self):
+    def getAllCoords(self) -> list[Coords]:
         return list(self._map.keys())
 
-    def getMinCoords(self):
+    def getMinCoords(self) -> list[Coord]:
         return self._minCoords
 
-    def getMaxCoords(self):
+    def getMaxCoords(self) -> list[Coord]:
         return self._maxCoords
 
-    def addMark(self, coords, mark=None):
+    def addMark(self, coords: Coords, mark: Optional[str] = None) -> None:
         self._validateCoords(coords)
         mark = self._validateMark(mark)
 
@@ -99,22 +125,28 @@ class SparseGrid:
 
         self._marked[coords][mark] = True
 
-    def clearMark(self, coords, mark=None):
+    def clearMark(self, coords: Coords, mark: Optional[str] = None) -> None:
         self._validateCoords(coords)
         mark = self._validateMark(mark)
 
-        if coords in self._marked and mark in self._marked[index]:
+        if coords in self._marked and mark in self._marked[coords]:
             del self._marked[coords][mark]
 
-    def clearAllMarks(self):
+    def clearAllMarks(self) -> None:
         self._marked = {}
 
-    def hasMark(self, coords, mark=None):
+    def hasMark(self, coords: Coords, mark: Optional[str] = None) -> bool:
         mark = self._validateMark(mark)
 
         return coords in self._marked and mark in self._marked[coords]
 
-    def print2D(self, minCoords=None, maxCoords=None, sep='', default=None):
+    def print2D(
+        self,
+        minCoords: Optional[list[Coord]] = None,
+        maxCoords: Optional[list[Coord]] = None,
+        sep: str = '',
+        default: Any = None,
+    ) -> None:
         assert self._dimension == 2, 'Cannot print2D with dimension: %d' % self._dimension
 
         if not minCoords:
@@ -131,7 +163,13 @@ class SparseGrid:
             print()
         print()
 
-    def print2DSlices(self, minCoords=None, maxCoords=None, sep='', default=None):
+    def print2DSlices(
+        self,
+        minCoords: Optional[list[Coord]] = None,
+        maxCoords: Optional[list[Coord]] = None,
+        sep: str = '',
+        default: Any = None,
+    ) -> None:
         assert self._dimension == 3, 'Cannot print2DSlices with dimension: %d' % self._dimension
 
         if not minCoords:
@@ -151,17 +189,29 @@ class SparseGrid:
             print()
         print()
 
-    def _validateCoords(self, coords):
+    def _validateCoords(self, coords: Sized) -> None:
         assert len(coords) == self._dimension, 'Coords have wrong dimension: %s' % str(coords)
 
-    def _updateBoundaryCoords(self, coords):
+    def _assertBoundaryCoords(self) -> None:
+        assert len(self._minCoords) > 0 and len(self._maxCoords) > 0, \
+            'max and min coords have not been initialized'
+
+    def _updateBoundaryCoords(self, coords: Coords) -> None:
         self._validateCoords(coords)
+
+        if len(self._minCoords) == 0 and len(self._maxCoords) == 0:
+            # Initializing boundary conditions.
+            self._minCoords = list(coords)
+            self._maxCoords = list(coords)
+            return
+
+        self._assertBoundaryCoords()
         for d in range(0, len(coords)):
             v = coords[d]
-            if self._minCoords[d] == None or v < self._minCoords[d]:
+            if v < self._minCoords[d]:
                 self._minCoords[d] = v
-            if self._maxCoords[d] == None or v > self._maxCoords[d]:
+            if v > self._maxCoords[d]:
                 self._maxCoords[d] = v
 
-    def _validateMark(self, mark):
-        return mark if mark != None else self._MARKED
+    def _validateMark(self, mark: Optional[str]) -> str:
+        return mark if mark is not None else _MARKED
