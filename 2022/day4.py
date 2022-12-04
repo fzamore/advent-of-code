7627e1e28fb97be947904f6f3f878f99ddcf006a
Eric Zamore <fzamore@gmail.com>
2022-12-04 14:20:09 -0500

    adding typehints to day4

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,14 +1,14 @@
-def completelyContains(outer: tuple, inner: tuple):
+def completelyContains(outer: tuple[int, int], inner: tuple[int, int]) -> bool:
   return outer[0] <= inner[0] and outer[1] >= inner[1]
 
-def overlaps(e1: tuple, e2: tuple):
+def overlaps(e1: tuple[int, int], e2: tuple[int, int]) -> bool:
   return e2[0] <= e1[1] and e1[0] <= e2[1]
 
 def part1():
   count = 0
   for line in open('day4.txt').read().splitlines():
     # each elf is a (x,y) range tuple
-    e1, e2 = [tuple(map(int, s.split('-'))) for s in line.split(',')]
+    e1, e2 = (tuple(map(int, s.split('-'))) for s in line.split(','))
     if completelyContains(e1, e2) or completelyContains(e2, e1):
       count += 1
   print(count)
@@ -17,7 +17,7 @@ def part2():
   count = 0
   for line in open('day4.txt').read().splitlines():
     # each elf is a (x,y) range tuple
-    e1, e2 = [tuple(map(int, s.split('-'))) for s in line.split(',')]
+    e1, e2 = (tuple(map(int, s.split('-'))) for s in line.split(','))
     if overlaps(e1, e2):
       count += 1
   print(count)
