b32a7b3d178b985f7a37117211390a8964607cc8
Eric Zamore <fzamore@gmail.com>
2022-12-09 10:06:11 -0500

    day9 done

---
+++ 2022/day9.py
@@ -0,0 +1,85 @@
+input = open('day9.txt').read().splitlines()
+
+Coords =  tuple[int, int]
+Rope = list[Coords]
+ROPE_LEN  = 10
+
+def getDelta(dir: str) -> Coords:
+  assert len(dir) == 1, 'invalid direction: %s' % dir
+  return {
+    'U': (0, -1),
+    'D': (0, 1),
+    'L': (-1, 0),
+    'R': (1, 0),
+  }[dir]
+
+def getDist(c1: Coords, c2: Coords) -> int:
+  return max(abs(c2[0] - c1[0]), abs(c2[1] -  c1[1]))
+
+def clamp(n: int, smallest: int, largest: int) -> int:
+  return max(smallest, min(n, largest))
+
+def computeTailDelta(nhpos: Coords, tpos: Coords) -> Coords:
+  # Move the tail toward the head such that the tail moves at most one
+  # unit in each direction.
+  return (
+    clamp(nhpos[0] - tpos[0], -1, 1),
+    clamp(nhpos[1] - tpos[1], -1, 1),
+  )
+
+def step(hpos: Coords, tpos: Coords, delta: Coords) -> tuple[Coords, Coords]:
+  nhpos = (hpos[0] + delta[0], hpos[1] + delta[1])
+  dist = getDist(nhpos, tpos)
+  if dist <= 1:
+    # The head hasn't moved far enough to drag the tail anywhere
+    return (nhpos, tpos)
+
+  assert dist == 2, 'invalid movement: %s, %s, %s' % (hpos, tpos, delta)
+  tdelta = computeTailDelta(nhpos, tpos)
+  return (nhpos, (tpos[0] + tdelta[0], tpos[1] + tdelta[1]))
+
+def stepRope(rope: Rope, delta: Coords) -> Rope:
+  nrope = rope.copy()
+  # first, move the head and knot 1, by assuming knot 0 is the head and
+  # knot 1 is the tail
+  nrope[0], nrope[1] = step(nrope[0], nrope[1], delta)
+
+  # next, move knots 2-9
+  for i in range(2, ROPE_LEN):
+    dist = getDist(nrope[i - 1], nrope[i])
+    if dist <= 1:
+      # knot i hasn't moved far enough to cause knot i - 1 to move
+      break
+    tdelta = computeTailDelta(nrope[i - 1], nrope[i])
+    nrope[i] = (nrope[i][0] + tdelta[0], nrope[i][1] + tdelta[1])
+
+  return nrope
+
+def part1():
+  hpos = (0, 0)
+  tpos = (0, 0)
+  allTpos = {tpos}
+  for line in input:
+    values = line.split()
+    delta, qty = getDelta(values[0]), int(values[1])
+    print(values)
+    for i in range(qty):
+      hpos, tpos = step(hpos, tpos, delta)
+      allTpos.add(tpos)
+    print(hpos, tpos)
+  print(len(allTpos))
+
+def part2():
+  rope = [(0, 0)] * ROPE_LEN
+  allTpos = {rope[-1]}
+  for line in input:
+    values = line.split()
+    delta, qty = getDelta(values[0]), int(values[1])
+    print(values)
+    for i in range(qty):
+      rope = stepRope(rope, delta)
+      allTpos.add(rope[-1])
+    print(rope)
+  print(len(allTpos))
+
+part2()
