a2def97c0c9e14b739a0fa273978fefe6bd4fb6c
Eric Zamore <fzamore@gmail.com>
2022-12-10 11:54:54 -0500

    day10 part 2 done

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,3 +1,5 @@
+GRID_WIDTH = 40
+
 input = open('day10.txt').read().splitlines()
 
 def getCyclesPerInstr(instr: str) -> int:
@@ -6,11 +8,15 @@ def getCyclesPerInstr(instr: str) -> int:
     'addx': 2,
   }[instr]
 
-def getValueAtCycle(cycles: list[int], cycle: int) -> int:
-  return cycles[cycle - 1]
-
 def getSignalStrengthAtCycle(cycles: list[int], cycle: int) -> int:
-  return getValueAtCycle(cycles, cycle) * cycle
+  valueAtCycle = cycles[cycle - 1]
+  return valueAtCycle * cycle
+
+def printPixel(px: int, x: int) -> None:
+  if x - 1 <= px <= x + 1:
+    print('#', end='')
+  else:
+    print('.', end='')
 
 def part1():
   x = 1
@@ -25,5 +31,20 @@ def part1():
   print(sum([getSignalStrengthAtCycle(cycles, i)
     for i in [20, 60, 100, 140, 180, 220]]))
 
+def part2():
+  px = 0
+  x = 1
+  for line in input:
+    tokens = line.split()
+    instr = tokens[0]
+    for _ in range(getCyclesPerInstr(instr)):
+      printPixel(px, x)
+      px += 1
+      if px == GRID_WIDTH:
+        # end of line. print a newline
+        print()
+        px = 0
+    if instr == 'addx':
+      x += int(tokens[1])
 
-part1()
+part2()
