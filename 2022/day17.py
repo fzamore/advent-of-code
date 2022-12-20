83580d107f4d15a89ea9ec611bb020620b2f0005
Eric Zamore <fzamore@gmail.com>
2022-12-20 11:31:25 -0500

    day17 part 2 done

--- 2022/day17.py
+++ 2022/day17.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 from common.sparsegrid import SparseGrid
 
 input = open('day17.txt').read().splitlines()[0]
@@ -11,6 +11,13 @@ Rock = namedtuple('Rock', [
   'left', # leftmost point, relative to bottom
   'top', # top point, relative to bottom
 ])
+rocks = [
+  Rock([(0, 0), (1, 0), (2, 0), (3, 0)], (0, 0), (0, 0)),
+  Rock([(0, 0), (-1, 1), (0, 1), (1, 1), (0, 2)], (-1, 1), (0, 2)),
+  Rock([(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)], (0, 0), (2, 2)),
+  Rock([(0, 0), (0, 1), (0, 2), (0, 3)], (0, 0), (0, 3)),
+  Rock([(0, 0), (1, 0), (0, 1), (1, 1)], (0, 0), (0, 1)),
+]
 
 def computeRockPositions(
   rock: Rock,
@@ -64,9 +71,12 @@ def printCave(
   cave: SparseGrid,
   height: int,
   fallingRockPositions: list[tuple[int, int]] = [],
-  ) -> None:
+  yBounds: tuple[int, int] = (-1, -1),
+) -> None:
+  if yBounds[1] == -1:
+    yBounds = (-1, height - 1)
   print()
-  for y in range(height - 1, -2, -1):
+  for y in range(yBounds[1], yBounds[0] - 1, -1):
     for x in range(-1, WIDTH + 1):
       if x == -1 or x == WIDTH:
         if y == -1:
@@ -81,15 +91,28 @@ def printCave(
     print()
   print()
 
-def part1():
-  rocks = [
-    Rock([(0, 0), (1, 0), (2, 0), (3, 0)], (0, 0), (0, 0)),
-    Rock([(0, 0), (-1, 1), (0, 1), (1, 1), (0, 2)], (-1, 1), (0, 2)),
-    Rock([(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)], (0, 0), (2, 2)),
-    Rock([(0, 0), (0, 1), (0, 2), (0, 3)], (0, 0), (0, 3)),
-    Rock([(0, 0), (1, 0), (0, 1), (1, 1)], (0, 0), (0, 1)),
-  ]
+# Returns the contour at the top of the cave. Return value is always a
+# seven-length tuple, the first element is always 0. The next six elements
+# are the y-positions of the top of of the cave at that x-position,
+# relative to the first element.
+def getCaveTopPattern(
+  cave: SparseGrid,
+  caveTop: int,
+) -> tuple[int, ...]:
+  # Leftmost value is always 0, and everything after is relative to that.
+  result = [0]
+  for x in range(0, WIDTH):
+    y = caveTop
+    while not cave.hasValue((x, y)):
+      y -= 1
+    if x == 0:
+      baseY = y
+    else:
+      result.append(y - baseY)
+  assert len(result) == 7
+  return tuple(result)
 
+def part1():
   cave = SparseGrid(2)
   caveTop = -1
   # Add bottom of cave.
@@ -124,4 +147,113 @@ def part1():
 
   print(caveTop + 1)
 
-part1()
+def part2():
+  patternLength = len(input)
+  print('jet pattern length:', patternLength)
+
+  cave = SparseGrid(2)
+  caveTop = -1
+  # Add bottom of cave.
+  for i in range(WIDTH):
+    cave.setValue((i, -1), '-')
+
+  # The cave top pattens. The key is described below.
+  caveTopPatterns = {}
+
+  # The cave top after each rock lands.
+  caveTops = {}
+
+  jpos = 0
+  print()
+  i = 0
+  while True:
+    rock = rocks[i % len(rocks)]
+    bottomPoint = (2 - rock.left[0], caveTop + 4)
+    while True:
+      # Move via the jet.
+      jet = input[jpos % len(input)]
+      jpos += 1
+      assert jet in ['<', '>'], 'bad jet: %s' % jet
+      delta = {'<': (-1, 0), '>': (1, 0)}[jet]
+      bottomPoint = tryMoveRock(cave, rock, bottomPoint, delta)
+
+      # Move via gravity.
+      delta = (0, -1)
+      np = tryMoveRock(cave, rock, bottomPoint, (0, -1))
+      if np == bottomPoint:
+        # The rock has come to rest.
+        storeRock(cave, rock, bottomPoint)
+
+        rockHeight = rock.top[1] + 1
+        # The top should never shrink.
+        caveTop = max(caveTop, rockHeight + bottomPoint[1] - 1)
+        caveTops[i] = caveTop
+        break
+      bottomPoint = np
+
+    # The goal is to find a recurring pattern, given a unique combination of:
+    # - rock number
+    # - jet position
+    # - cave-top pattern
+    # If these three things are the same, then we will have a repeating pattern.
+    # Given such a pattern, we want to determine:
+    # - the configuration at the start of the pattern
+    # - how many rocks are dropped during each pattern
+    # - most importantly: how far the top of the cave rises during each pattern
+    caveTopPatternKey = (
+      i % len(rocks),
+      jpos % len(input),
+      getCaveTopPattern(cave, caveTop),
+    )
+    if caveTopPatternKey in caveTopPatterns:
+      # We have found the end of the first occurrence of the pattern. Stop
+      # dropping rocks.
+      break
+    # Store the rock index for this pattern at each dropped rock.
+    caveTopPatterns[caveTopPatternKey] = i
+
+    i += 1
+
+  # The rock index at the start of the pattern.
+  firstRockI = caveTopPatterns[caveTopPatternKey] + 1
+  # The rock index at the end of the pattern.
+  lastRockI = i
+  rocksPerPattern = lastRockI - firstRockI + 1
+  print('firstRockI:', firstRockI)
+  print('lastRockI:', lastRockI)
+  print('rocksPerPattern:', rocksPerPattern)
+  assert len(caveTops) >= rocksPerPattern
+
+  # The amount the cave top rises per pattern.
+  assert caveTops[i] == caveTop
+  caveTopChangePerPattern = caveTops[i] - caveTops[firstRockI - 1]
+  print('caveTopChangePerPattern:', caveTopChangePerPattern)
+
+  totalRocks = 1000000000000
+
+  # The number of complete patterns we need.
+  numCompletePatterns = totalRocks // rocksPerPattern
+  # The number of rocks we need to drop after the last pattern has completed.
+  leftOverRocks = totalRocks % rocksPerPattern
+
+  # The top of the cave at the end of the last complete pattern.
+  caveTopAtEndOfLastPattern = numCompletePatterns * caveTopChangePerPattern
+
+  print('numPatterns:', numCompletePatterns)
+  print('leftOverRocks:', leftOverRocks)
+  print('caveTopAtTopOfPattern:', caveTopAtEndOfLastPattern)
+
+  assert rocksPerPattern * numCompletePatterns + leftOverRocks == totalRocks
+
+  print(
+    caveTops[leftOverRocks],
+    caveTops[firstRockI + leftOverRocks] - caveTops[firstRockI],
+  )
+
+  # I don't understand why this is the correct calculation.
+  # I think it should be:
+  #   caveTopAtEndOfLastPattern +
+  #     caveTops[firstRockI + leftOverRocks] - caveTops[firstRockI]
+  print(caveTopAtEndOfLastPattern + caveTops[leftOverRocks])
+
+part2()
