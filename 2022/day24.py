19b21c11059d72b686f1c4b0648915bb566afec6
Eric Zamore <fzamore@gmail.com>
2022-12-26 16:50:52 -0500

    day 24 part 2 done

--- 2022/day24.py
+++ 2022/day24.py
@@ -5,7 +5,7 @@ input = open('day24.txt').read().splitlines()
 
 Coords = tuple[int, int]
 Delta = tuple[int, int]
-Node = tuple[Coords, int]
+Node = tuple[Coords, int] # ((x, y), elapsedTime)
 
 def createEmptyGrid(width: int, height: int) -> ArrayGrid:
   grid = ArrayGrid(width, height)
@@ -76,6 +76,44 @@ def iterateBlizzards(grid: ArrayGrid) -> ArrayGrid:
 
   return ngrid
 
+def getAdjacentNodes(
+  node: Node,
+  gridIterations: list[ArrayGrid],
+  startPos: Coords,
+  endPos: Coords | None = None,
+) -> list[tuple[Node, int]]:
+  (x, y), minutesElapsed = node
+
+  if minutesElapsed >= len(gridIterations):
+    # Compute the state of the grid at time `minutesElapsed + 1`.
+    for i in range(len(gridIterations), minutesElapsed + 1):
+      gridIterations.append(iterateBlizzards(gridIterations[i - 1]))
+  grid = gridIterations[minutesElapsed]
+  assert grid is not None
+
+  neighbors = []
+  for dx, dy in [(1, 0), (0, 1), (0, 0), (-1, 0), (0, -1)]:
+    nx, ny = x + dx, y + dy
+    # special-case waiting in the start position (because its position
+    # is technically outside the grid)
+    if (nx, ny) in [startPos, endPos] and (dx, dy) == (0, 0):
+      neighbors.append((nx, ny))
+      continue
+    if not grid.areCoordsWithinBounds(nx, ny):
+      continue
+    if len(grid.getValue(nx, ny)) == 0:
+      neighbors.append((nx, ny))
+
+  return [((pos, minutesElapsed + 1), 1) for pos in neighbors]
+
+def isDone(node: Node, width: int, height: int) -> bool:
+  (x, y), _ = node
+  return x == width - 1 and y == height - 1
+
+def isDone2(node: Node, width: int, height: int) -> bool:
+  (x, y), _ = node
+  return x == 0 and y == 0
+
 def part1():
   grid = initGrid()
 
@@ -89,48 +127,75 @@ def part1():
   # neighbors, get the state of the grid at time t + 1, and return all
   # possible moves in that grid (including not changing positions).
 
-  # precompute grid iterations (400 is an upper bound determined by
-  # previous failed approaches).
-  gridIterations: dict[int, ArrayGrid] = {
-     0: grid,
-  }
-  for i in range(1, 400):
-    gridIterations[i] = iterateBlizzards(gridIterations[i - 1])
+  # Iterations of the grid, indexed by elapsed time.
+  gridIterations = [grid]
 
-  def getAdjacent(node: Node) -> list[tuple[Node, int]]:
-    (x, y), minutesElapsed = node
-
-    grid = gridIterations[minutesElapsed]
-    assert grid is not None
-
-    neighbors = []
-    for dx, dy in [(1, 0), (0, 1), (0, 0), (-1, 0), (0, -1)]:
-      nx, ny = x + dx, y + dy
-      # special-case waiting in the start position (because its position
-      # is technically outside the grid)
-      if (nx, ny) == startPos and (dx, dy) == (0, 0):
-        neighbors.append((nx, ny))
-        continue
-
-      if not grid.areCoordsWithinBounds(nx, ny):
-        continue
-
-      if len(grid.getValue(nx, ny)) == 0:
-        neighbors.append((nx, ny))
-
-    return [((pos, minutesElapsed + 1), 1) for pos in neighbors]
-
-  def isDone(node: Node) -> bool:
-    (x, y), _ = node
-    return x == width - 1 and y == height - 1
+  getAdjacent = lambda node: getAdjacentNodes(
+    node,
+    gridIterations,
+    startPos,
+  )
 
   ((x, y), _), score = dijkstra(
     (startPos, 0),
     getAdjacent,
-    isDone,
+    lambda node: isDone(node, width, height),
   )
   print(x, y)
   print(score)
 
-part1()
+def part2():
+  grid = initGrid()
 
+  width, height = grid.getWidth(), grid.getHeight()
+  print('width x height:', width, height)
+
+  startPos = (0, -1)
+  endPos = (width - 1, height)
+
+  print('startPos:', startPos)
+  print('endPos:', endPos)
+
+  # Approach: run Dijkstra over the grid, but each node is an (x, y, t)
+  # tuple, where t is the minutes elapsed so far. When determining
+  # neighbors, get the state of the grid at time t + 1, and return all
+  # possible moves in that grid (including not changing positions).
+
+  # Iterations of the grid, indexed by elapsed time.
+  gridIterations = [grid]
+
+  getAdjacent = lambda node: getAdjacentNodes(
+    node,
+    gridIterations,
+    startPos,
+    endPos,
+  )
+
+  time = 0
+  _, diff = dijkstra(
+    (startPos, time),
+    getAdjacent,
+    lambda node: isDone(node, width, height),
+  )
+  time += diff
+  print('diff:', diff, 'total:', time)
+
+  _, diff = dijkstra(
+    (endPos, time),
+    getAdjacent,
+    lambda node: isDone2(node, width, height),
+  )
+  time += diff
+  print('diff:', diff, 'total:', time)
+
+  _, diff = dijkstra(
+    (startPos, time),
+    getAdjacent,
+    lambda node: isDone(node, width, height),
+  )
+  time += diff
+  print('diff:', diff, 'total:', time)
+
+  print(time)
+
+part2()
