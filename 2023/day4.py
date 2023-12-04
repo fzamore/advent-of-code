fb31987efb9c7c99c591ade83d2ad346b8c4ee12
Eric Zamore <fzamore@gmail.com>
2023-12-04 11:26:05 -0500

    day4 part 2 done

--- 2023/day4.py
+++ 2023/day4.py
@@ -26,4 +26,40 @@ def part1():
       sum += pow(2, len(matches) - 1)
   print(sum)
 
-part1()
+def part2():
+  cards = [parseLine(line) for line in input]
+  print(cards)
+  print(len(cards))
+
+  cardScores = {}
+  cardsInHand = {}
+  for card in cards:
+    s = set(card.winningNumbers)
+    matches = [n for n in card.myNumbers if n in s]
+    score = len(matches)
+    cardScores[card.id] = score
+
+    cardsInHand[card.id] = 1
+
+  print(cardsInHand)
+  print(cardScores)
+
+  maxId = max(cardScores.keys())
+  print('maxId:', maxId)
+  print()
+
+  # Process cards in order, since card X can only affect cards > X.
+  for cardId in range(1, maxId + 1):
+    score = cardScores[cardId]
+    if score == 0:
+      continue
+    assert cardId + score <= maxId, 'bad card score'
+    for i in range(cardId + 1, cardId + score + 1):
+      # For each matching card, add as many copies as we have of the
+      # current card.
+      cardsInHand[i] += cardsInHand[cardId]
+
+  print(cardsInHand)
+  print(sum(cardsInHand.values()))
+
+part2()
