ac9b6ac3bbedcaac4f25c12596ed0f94656df75c
Eric Zamore <fzamore@gmail.com>
2023-12-13 10:25:33 -0500

    day13 part 2 done

--- 2023/day13.py
+++ 2023/day13.py
@@ -13,7 +13,6 @@ def initGrids() -> list[ArrayGrid]:
       for x in range(w):
         grid.setValue(x, y, gridLines[y][x])
     grids.append(grid)
-    grid.print2D()
   return grids
 
 def areRowsEqual(grid: ArrayGrid, y1: int, y2: int) -> bool:
@@ -54,6 +53,59 @@ def isVertReflection(grid: ArrayGrid, x: int) -> bool:
       return False
   return True
 
+def findHorizReflection(grid: ArrayGrid, origReflection: int = -1) -> int:
+  reflection = -1
+  for y in range(grid.getHeight() - 1):
+    if y == origReflection:
+      continue
+    if isHorizReflection(grid, y):
+      assert reflection == -1, \
+        'more than one horiz reflection found: %d, %d' % (y, reflection)
+      reflection = y
+  return reflection
+
+def findVertReflection(grid: ArrayGrid, origReflection: int = -1) -> int:
+  reflection = -1
+  for x in range(grid.getWidth() - 1):
+    if x == origReflection:
+      continue
+    if isVertReflection(grid, x):
+      assert reflection == -1, \
+        'more than one vert reflection found: %d, %d' % (x, reflection)
+      reflection = x
+  return reflection
+
+def smudgeAndfindReflections(
+    grid: ArrayGrid,
+    origHorizReflection: int,
+    origVertReflection: int,
+  ) -> tuple[int, int]:
+  for y in range(grid.getHeight()):
+    for x in range(grid.getWidth()):
+      # smudge!
+      v = grid.getValue(x, y)
+      nv = '#' if v == '.' else '.'
+      grid.setValue(x, y, nv)
+
+      horizReflection = findHorizReflection(grid, origHorizReflection)
+      if horizReflection != -1:
+        print('new horiz:', horizReflection)
+        # unsmudge
+        grid.setValue(x, y, v)
+        return (horizReflection, -1)
+
+      vertReflection = findVertReflection(grid, origVertReflection)
+      if vertReflection != -1:
+        print('new vert:', vertReflection)
+        vertReflection = vertReflection
+        # unsmudge
+        grid.setValue(x, y, v)
+        return (-1, vertReflection)
+      # unsmudge
+      grid.setValue(x, y, v)
+
+  assert False, 'did not find new horiz or vert reflection'
+
 def part1():
   grids = initGrids()
 
@@ -61,21 +113,10 @@ def part1():
 
   r = 0
   for grid in grids:
-    horizReflection = -1
-    for y in range(grid.getHeight() - 1):
-      if isHorizReflection(grid, y):
-        assert horizReflection == -1, \
-          'more than one horiz reflection found: %d, %d' % (y, horizReflection)
-        horizReflection = y
-
-    vertReflection = -1
-    for x in range(grid.getWidth() - 1):
-      if isVertReflection(grid, x):
-        assert vertReflection == -1, \
-          'more than one vert reflection found: %d, %d' % (x, vertReflection)
-        vertReflection = x
-
+    horizReflection = findHorizReflection(grid)
+    vertReflection = findVertReflection(grid)
     print(horizReflection, vertReflection)
+
     assert \
       (horizReflection != -1 or vertReflection != -1) and \
       (horizReflection == -1 or vertReflection == -1), \
@@ -85,4 +126,38 @@ def part1():
     r += 100 * (horizReflection + 1) + vertReflection + 1
   print(r)
 
-part1()
+def part2():
+  grids = initGrids()
+
+  print('grid count:', len(grids))
+
+  r = 0
+  for grid in grids:
+    print()
+    horizReflection = -1
+    vertReflection = -1
+    origHorizReflection = findHorizReflection(grid)
+    origVertReflection = findVertReflection(grid)
+    print('initial:', origHorizReflection, origVertReflection)
+    assert \
+      (origHorizReflection != -1 or origVertReflection != -1) and \
+      (origHorizReflection == -1 or origVertReflection == -1), \
+      'exactly one horiz or vert should have been found: %d, %d' \
+        % (origHorizReflection, origVertReflection)
+
+    horizReflection, vertReflection = smudgeAndfindReflections(
+      grid,
+      origHorizReflection,
+      origVertReflection,
+    )
+
+    assert \
+      (horizReflection != -1 or vertReflection != -1) and \
+      (horizReflection == -1 or vertReflection == -1), \
+      'exactly one horiz or vert should have been found: %d, %d' \
+        % (horizReflection, vertReflection)
+
+    r += 100 * (horizReflection + 1) + vertReflection + 1
+  print(r)
+
+part2()
