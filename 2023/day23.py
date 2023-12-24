ee79203ef1644a9d907546b4aae234a671493e53
Eric Zamore <fzamore@gmail.com>
2023-12-23 21:25:36 -0500

    day23 part 1 speedup from 18s -> 0.9s

--- 2023/day23.py
+++ 2023/day23.py
@@ -1,9 +1,5 @@
-import sys
 from typing import Optional
 from common.arraygrid import ArrayGrid
-
-# I should probably find a more efficient solution.
-sys.setrecursionlimit(4000)
 
 input = open('day23.txt').read().splitlines()
 
@@ -26,37 +22,59 @@ def canMove(cur: Coords, next: Coords, nextValue: str) -> bool:
 
   return (dx, dy) == allowed[nextValue]
 
+def getAdjacentCoords(
+  grid: ArrayGrid,
+  cur: Coords,
+  prev: Optional[Coords],
+  visited: set[Coords],
+) -> list[Coords]:
+  results = []
+  x, y = cur
+  for next in grid.getAdjacentCoords(x, y):
+    if next == prev or next in visited:
+      continue
+    nx, ny = next
+    if canMove(cur, next, grid.getValue(nx, ny)):
+      results.append(next)
+  return results
+
 def findLongestPath(
   grid: ArrayGrid,
   cur: Coords,
+  prev: Optional[Coords],
   end: Coords,
   visited: set[Coords] = set(),
 ) -> Optional[list[Coords]]:
   assert cur not in visited, 'already visited node: %s' % str(cur)
   visited.add(cur)
-
   path = [cur]
+
+  # As long as there is exactly one way to go, keep moving that way
+  # without recurring (to not increase the recursion stack depth).
+  adj = getAdjacentCoords(grid, cur, prev, visited)
+  while len(adj) == 1:
+    prev = cur
+    cur = adj[0]
+    path.append(cur)
+    visited.add(cur)
+    adj = getAdjacentCoords(grid, cur, prev, visited)
 
   if cur == end:
     return path
 
   longestLen = -1
   longestPath = None
-  x, y = cur
-  for next in grid.getAdjacentCoords(x, y):
-    if next in visited:
+  for next in adj:
+    npath = findLongestPath(grid, next, cur, end, visited.copy())
+    if npath is None:
       continue
-    nx, ny = next
-    if canMove(cur, next, grid.getValue(nx, ny)):
-      npath = findLongestPath(grid, next, end, visited.copy())
-      if npath is None:
-        continue
-      if len(npath) > longestLen:
-        longestLen = len(npath)
-        longestPath = npath.copy()
+    if len(npath) > longestLen:
+      longestLen = len(npath)
+      longestPath = npath.copy()
 
   if longestPath is None:
     return None
+
   path.extend(longestPath)
   return path
 
@@ -64,12 +82,14 @@ def part1() -> None:
   grid = ArrayGrid.gridFromInput(input)
   w, h, = grid.getWidth(), grid.getHeight()
   print('grid: (%d x %d)' % (w, h))
+  grid.print2D()
 
   start = (1, 0)
   end = (w - 2, h - 1)
+  print('start:', start)
+  print('end:', end)
 
-  path = findLongestPath(grid, start, end)
-  print('longest path:', path)
+  path = findLongestPath(grid, start, None, end)
   if path is not None:
     for x, y in path:
       v = 'S' if (x, y) == start else 'O'
