e50b174c473ee2600dd505bdf1a0d67d4454ead5
Eric Zamore <fzamore@gmail.com>
2023-12-29 23:19:25 -0500

    day20 part 2 done

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,5 +1,6 @@
 from collections import defaultdict, namedtuple
 from enum import Enum
+from math import lcm
 
 input = open('day20.txt').read().splitlines()
 
@@ -12,79 +13,7 @@ Module = namedtuple('Module', ['name', 'type', 'value', 'dsts'])
 
 Pulse = namedtuple('Pulse', ['src', 'moduleName', 'value'])
 
-def pulseStr(pulse: Pulse) -> str:
-  src = pulse.src
-  value = '-high' if pulse.value else '-low'
-  output = pulse.moduleName
-  return '%s %s-> %s' % (src, value, output)
-
-def pulseModule(
-  modules: dict[str, Module],
-  inputValues: dict[str, dict[str, bool]],
-  name: str,
-  srcModule: str,
-  pulse: bool,
-) -> list[Pulse]:
-
-  if name not in modules:
-    # this module is untyped, thus it forwards no pulses
-    print('untyped module:', name)
-    return []
-
-  r = []
-  dsts = modules[name].dsts
-  type = modules[name].type
-  value = modules[name].value
-  match type:
-    case ModuleType.BROADCASTER:
-      for dst in dsts:
-        r.append(Pulse(name, dst, value))
-    case ModuleType.FLIP_FLOP:
-      if pulse:
-        return []
-      modules[name] = Module(name, type, not value, dsts)
-      for dst in dsts:
-        r.append(Pulse(name, dst, not value))
-    case ModuleType.CONJUNCTION:
-      inputValues[name][srcModule] = pulse
-      if False in inputValues[name].values():
-        output = True
-      else:
-        output = False
-      for dst in dsts:
-        r.append(Pulse(name, dst, output))
-      if len(inputValues[name]) > 1:
-        print('conj values:', name, inputValues[name], output)
-    case _:
-      assert False, 'bad module type'
-
-  return r
-
-def button(
-  modules: dict[str, Module],
-  inputValues: dict[str, dict[str, bool]],
-) -> tuple[int, int]:
-  highPulseCount = 0
-  lowPulseCount = 0
-  q = [Pulse('button', 'broadcaster', False)]
-  while len(q) > 0:
-    pulse = q.pop(0)
-    moduleName = pulse.moduleName
-    src = pulse.src
-    value = pulse.value
-
-    print(pulseStr(pulse))
-
-    if value:
-      highPulseCount += 1
-    else:
-      lowPulseCount +=1
-
-    q.extend(pulseModule(modules, inputValues, moduleName, src, value))
-
-  return highPulseCount, lowPulseCount
-
-def part1() -> None:
+def parseInput() -> tuple[dict[str, Module], dict[str, dict[str, bool]]]:
   modules = {}
   inputValues: dict[str, dict[str, bool]] = defaultdict(dict)
 
@@ -108,11 +37,91 @@ def part1() -> None:
     for dst in dsts:
       inputValues[dst][name] = False
 
-  # delete non-conjunctions from inputValues for easier debugging
-  keys = list(inputValues.keys())
-  for key in keys:
-    if key not in modules or modules[key].type != ModuleType.CONJUNCTION:
-      del inputValues[key]
+  return modules, inputValues
+
+def pulseStr(pulse: Pulse) -> str:
+  src = pulse.src
+  value = '-high' if pulse.value else '-low'
+  output = pulse.moduleName
+  return '%s %s-> %s' % (src, value, output)
+
+def pulseModule(
+  modules: dict[str, Module],
+  inputValues: dict[str, dict[str, bool]],
+  pulse: Pulse,
+  buttonPressCount: int,
+  conjunctionModulesToTrack: dict[str, int],
+) -> list[Pulse]:
+
+  name = pulse.moduleName
+  if name not in modules:
+    # this module is untyped, thus it forwards no pulses
+    return []
+
+  r = []
+  dsts = modules[name].dsts
+  type = modules[name].type
+  value = modules[name].value
+  match type:
+    case ModuleType.BROADCASTER:
+      for dst in dsts:
+        r.append(Pulse(name, dst, value))
+    case ModuleType.FLIP_FLOP:
+      if pulse.value:
+        return []
+      modules[name] = Module(name, type, not value, dsts)
+      for dst in dsts:
+        r.append(Pulse(name, dst, not value))
+    case ModuleType.CONJUNCTION:
+      inputValues[name][pulse.src] = pulse.value
+      if False in inputValues[name].values():
+        output = True
+
+        if name in conjunctionModulesToTrack:
+          # This is one of the conjunction modules we're interested in
+          # tracking when it emits a high pulse. Record how many button
+          # presses it took to get too thiss point.
+          assert len(inputValues[name]) == 1, 'bad conj module tracking'
+          conjunctionModulesToTrack[name] = buttonPressCount
+      else:
+        output = False
+      for dst in dsts:
+        r.append(Pulse(name, dst, output))
+    case _:
+      assert False, 'bad module type'
+
+  return r
+
+def pressButton(
+  modules: dict[str, Module],
+  inputValues: dict[str, dict[str, bool]],
+  buttonPressCount: int = -1,
+  conjunctionModulesToTrack: dict[str, int] = {}
+) -> tuple[int, int]:
+  highPulseCount = 0
+  lowPulseCount = 0
+  q = [Pulse('button', 'broadcaster', False)]
+  while len(q) > 0:
+    pulse = q.pop(0)
+    value = pulse.value
+    if value:
+      highPulseCount += 1
+    else:
+      lowPulseCount +=1
+
+    outputPulses = pulseModule(
+      modules,
+      inputValues,
+      pulse,
+      buttonPressCount,
+      conjunctionModulesToTrack,
+    )
+    q.extend(outputPulses)
+
+  return highPulseCount, lowPulseCount
+
+def part1() -> None:
+  modules, inputValues = parseInput()
 
   print(modules)
   print(inputValues)
@@ -123,7 +132,7 @@ def part1() -> None:
   high, low = 0, 0
   for i in range(c):
     print('button press', i)
-    h, l = button(modules, inputValues)
+    h, l = pressButton(modules, inputValues)
     high += h
     low += l
     print()
@@ -135,4 +144,48 @@ def part1() -> None:
   print(high, low)
   print(high * low)
 
-part1()
+def part2() -> None:
+  modules, inputValues = parseInput()
+
+  # This approach is heavily dependent on the following input structure.
+  #
+  # The target module is an untyped module (rx). This module has a single
+  # input (call it L1), which is a conjunction module. This conjunction
+  # module has four inputs (call them L2).
+  #
+  # To send a low pulse to the target, its input (L1) must receive all
+  # high pulses, which means all four conjunction modules in L2 need to
+  # emit high pulses in the same button press.
+  #
+  # It turns out that each L2 conjunction module emits a high pulse every
+  # X button presses, where X is different for each module. So, we find
+  # this value for each module in L2 and take the LCM of them all.
+
+  print('counts:', len(modules), len(inputValues))
+
+  target = 'rx'
+  print('target:', target)
+
+  # L1
+  assert len(inputValues[target]) == 1, 'target should only have one input'
+  targetInput = list(inputValues[target].keys())[0]
+  print('targetInput:', targetInput)
+  assert modules[targetInput].type == ModuleType.CONJUNCTION, 'bad targetInput type'
+
+  # L2
+  conjunctionModulesToTrack = dict((n, -1) for n in inputValues[targetInput])
+  print('conjunctionModulesToTrack:', conjunctionModulesToTrack)
+
+  i = 1
+  while True:
+    pressButton(modules, inputValues, i, conjunctionModulesToTrack)
+    i += 1
+
+    if -1 not in conjunctionModulesToTrack.values():
+      # If we've received a value for all relevant conjunction modules,
+      # we're done.
+      print('finished:', conjunctionModulesToTrack)
+      print(lcm(*conjunctionModulesToTrack.values()))
+      return
+
+part2()
