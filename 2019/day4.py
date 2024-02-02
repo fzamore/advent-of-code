a6fd5429fc0a5eb5e8379ba264ca07a2dea1b680
Eric Zamore <fzamore@gmail.com>
2024-02-02 16:06:25 -0500

    day4 part 2 done

--- 2019/day4.py
+++ 2019/day4.py
@@ -1,19 +1,32 @@
 v1, v2 = map(int, open('day4.txt').read().split('-'))
 
-def isValid(n: int) -> bool:
+def isValid(n: int, *, part2: bool = False) -> bool:
   ns = str(n)
   if len(ns) != 6:
     return False
 
   lastD = -1
   hasDouble = False
-  for c in ns:
-    d = int(c)
+  i = 0
+  while i < len(ns):
+    d = int(ns[i])
     if d < lastD:
       return False
-    if d == lastD:
-      hasDouble = True
+
+    adjCount = 1
+    j = i + 1
+    while j < len(ns) and int(ns[j]) == d:
+      adjCount += 1
+      j += 1
+
+    if not hasDouble:
+      if part2:
+        hasDouble = (adjCount == 2)
+      else:
+        hasDouble = (adjCount > 1)
+
     lastD = d
+    i = j
 
   return hasDouble
 
@@ -25,4 +38,12 @@ def part1():
       ans += 1
   print(ans)
 
-part1()
+def part2():
+  print('values:', v1, v2)
+  ans = 0
+  for r in range(v1, v2 + 1):
+    if isValid(r, part2=True):
+      ans += 1
+  print(ans)
+
+part2()
