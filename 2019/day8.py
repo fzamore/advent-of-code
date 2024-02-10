4430f954dab735ed770060ab2646bf8041c86677
Eric Zamore <fzamore@gmail.com>
2024-02-09 22:05:38 -0500

    day8 part2 done

--- 2019/day8.py
+++ 2019/day8.py
@@ -1,7 +1,20 @@
 from collections import defaultdict
 
-
 input = list(map(int, list(open('day8.txt').read().strip())))
+
+def valueForCoordinate(data: list[int], layerNum: int, x: int, y: int) -> int:
+  w = 25
+  h = 6
+
+  base = w * h * layerNum
+  return data[base + y * w + x]
+
+def pixelForCoordinate(data: list[int], x: int, y: int) -> str:
+  layerNum = 0
+  while (value := valueForCoordinate(data, layerNum, x, y)) == 2:
+    layerNum += 1
+  assert value in [0, 1], 'bad value'
+  return '#' if value == 1 else ' '
 
 def part1() -> None:
   w = 25
@@ -26,5 +39,16 @@ def part1() -> None:
 
   print(minLayer[1] * minLayer[2])
 
-part1()
+def part2() -> None:
+  w = 25
+  h = 6
+
+  print()
+  for y in range(h):
+    for x in range(w):
+      print(pixelForCoordinate(input, x, y), end='')
+    print()
+  print()
+
+part2()
 
