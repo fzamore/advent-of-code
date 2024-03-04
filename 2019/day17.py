91b5e69756337172c4ce5df8face807f4d1c5fa5
Eric Zamore <fzamore@gmail.com>
2024-03-04 12:04:32 -0500

    day17 part 1 done

---
+++ 2019/day17.py
@@ -0,0 +1,139 @@
+from enum import IntEnum
+
+from common.sparsegrid import SparseGrid
+
+input = open('day17.txt').read().split(',')
+
+class Op(IntEnum):
+  ADD: int = 1
+  MUL: int = 2
+  INPUT: int = 3
+  OUTPUT: int = 4
+  JUMP_IF_TRUE: int = 5
+  JUMP_IF_FALSE: int = 6
+  LESS_THAN: int = 7
+  EQUALS: int = 8
+  RELATIVE_BASE: int = 9
+
+  @staticmethod
+  def getParamCount(op: int) -> int:
+    return {
+      Op.ADD: 3,
+      Op.MUL: 3,
+      Op.INPUT: 1,
+      Op.OUTPUT: 1,
+      Op.JUMP_IF_TRUE: 2,
+      Op.JUMP_IF_FALSE: 2,
+      Op.LESS_THAN: 3,
+      Op.EQUALS: 3,
+      Op.RELATIVE_BASE: 1,
+   }[Op(op)]
+
+def getParameterAddresses(memory: dict[int, int], pc: int, relativeBase: int) -> list[int]:
+  value = memory[pc]
+  opcode = value % 100
+  paramModes = str(value // 100)[::-1] + '000' # tack on extra zeroes, which is the default
+  results = []
+  for i in range(Op.getParamCount(opcode)):
+    mode = paramModes[i]
+    match mode:
+      case '0':
+        address = memory.get(pc + i + 1, 0)
+      case '1':
+        address = pc + i + 1
+      case '2':
+        address = memory.get(pc + i + 1, 0) + relativeBase
+      case _:
+        assert False, 'bad mode: %s' % mode
+    results.append(address)
+  assert len(results) <= 3, 'bad addresses: %s' % results
+  return results
+
+def runMachine(memory: dict[int, int]) -> list[int]:
+  outputs = []
+  pc = 0
+  relativeBase = 0
+  while (value := memory[pc]) != 99:
+    opcode = value % 100
+    paramAddresses = getParameterAddresses(memory, pc, relativeBase)
+    paramValues = [memory.get(x, 0) for x in paramAddresses]
+    assert len(paramValues) == Op.getParamCount(opcode), \
+      'bad paramValues for opcode: %d, %s' % (opcode, paramValues)
+
+    # Asssume the destination is the last parameter, if it exists.
+    dst = paramAddresses[-1]
+    assert dst >= 0, 'dst cannot be negative'
+
+    match opcode:
+      case Op.ADD:
+        memory[dst] = paramValues[0] + paramValues[1]
+        pc += Op.getParamCount(opcode) + 1
+      case Op.MUL:
+        memory[dst] = paramValues[0] * paramValues[1]
+        pc += Op.getParamCount(opcode) + 1
+      case Op.INPUT:
+        assert False, 'not expecting input commands'
+      case Op.OUTPUT:
+        outputs.append(paramValues[0])
+        pc += Op.getParamCount(opcode) + 1
+      case Op.JUMP_IF_TRUE:
+        if paramValues[0] != 0:
+          pc = paramValues[1]
+        else:
+          pc += Op.getParamCount(opcode) + 1
+      case Op.JUMP_IF_FALSE:
+        if paramValues[0] == 0:
+          pc = paramValues[1]
+        else:
+          pc += Op.getParamCount(opcode) + 1
+      case Op.LESS_THAN:
+        memory[dst] = 1 if paramValues[0] < paramValues[1] else 0
+        pc += Op.getParamCount(opcode) + 1
+      case Op.EQUALS:
+        memory[dst] = 1 if paramValues[0] == paramValues[1] else 0
+        pc += Op.getParamCount(opcode) + 1
+      case Op.RELATIVE_BASE:
+        relativeBase += paramValues[0]
+        pc += Op.getParamCount(opcode) + 1
+      case _:
+        assert False, 'bad opcode: %s' % opcode
+
+  return outputs
+
+def part1() -> None:
+  memory = dict(zip(range(len(input)), list(map(int, input))))
+  outputs = runMachine(memory)
+
+  print('outputs', len(outputs))
+  for c in outputs:
+    print(chr(c), end='')
+
+  grid = SparseGrid(2)
+  x, y = 0, 0
+  for c in outputs:
+    if c == 10:
+      x = 0
+      y += 1
+    else:
+      grid.setValue((x, y), chr(c))
+      x += 1
+  grid.print2D()
+
+  w, h = grid.getMaxCoords()
+  print('w x h:', w, h)
+
+  intersections = []
+  for x, y in grid.getAllCoords():
+    isIntersection = True
+    for dx, dy in [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)]:
+      if grid.getValue((x + dx, y + dy), '.') != '#':
+        isIntersection = False
+        break
+    if isIntersection:
+      intersections.append((x, y))
+
+  print('intersections:', len(intersections))
+  print(sum([x * y for x, y in intersections]))
+
+
+part1()
