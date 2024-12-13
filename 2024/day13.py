48444f27fa733a2fafa779ba498bddce3a88c77c
Eric Zamore <fzamore@gmail.com>
2024-12-13 09:10:59 -0500

    day13 done. suspiciously easy

--- 2024/day13.py
+++ 2024/day13.py
@@ -7,44 +7,54 @@ input = open('day13.txt').read().splitlines()
 Button = tuple[int, int]
 Machine = namedtuple('Machine', ['a', 'b', 'prize'])
 
-def parseInput() -> list[Machine]:
+def parseInput(delta: int = 0) -> list[Machine]:
   machines = []
   i = 0
   while i < len(input):
     machines.append(Machine(
       tuple(ints(input[i])),
       tuple(ints(input[i + 1])),
-      tuple(ints(input[i + 2])),
+      tuple([i + delta for i in ints(input[i + 2])]),
     ))
     i += 4
   return machines
 
-def findMinTokens(machine: Machine, n: int = 100) -> Optional[int]:
-  # Equations:
+def findMinTokens(machine: Machine) -> Optional[int]:
+  # This is simple constant time algebra.
+  #
+  # Initial equations:
   #   a * ax + b * bx = px
   #   a * ay + b * by = py
+  #
+  # Solving for a and b:
+  #   a = (px - b * bx) / ax
+  #   b = (px - a * ax) / bx
+  #
+  # Substituting / solving for b:
+  #   b = (px * ay - py * ax) / (bx * ay - by * ax)
+  #
+  # Everything is known except a and b.
+
   (ax, ay), (bx, by), (px, py) = machine
-  best = 10000
-  foundAny = False
-  for i in range(n):
-    a = i
-    # b = (px - a * ax) / bx
-    numerator = px - a * ax
-    if numerator % bx != 0:
-      continue
 
-    b = numerator // bx
+  # Solve for b.
+  numerator = px * ay - py * ax
+  denominator = bx * ay - by * ax
+  assert denominator != 0, 'bad denominator'
+  if numerator % denominator != 0:
+    return None
 
-    if a * ay + b * by != py:
-      continue
+  b = numerator // denominator
 
-    if a > 100 or b > 100:
-      continue
+  # Use b to solve for a.
+  numerator = px - b * bx
+  if numerator % ax != 0:
+    return None
 
-    foundAny = True
-    best = min(best, a * 3 + b)
+  a = numerator // ax
 
-  return best if foundAny else None
+  # Three tokens for a, one token for b.
+  return a * 3 + b
 
 def part1() -> None:
   machines = parseInput()
@@ -53,4 +63,11 @@ def part1() -> None:
   ans = sum([t for t in map(findMinTokens, machines) if t is not None])
   print(ans)
 
-part1()
+def part2() -> None:
+  machines = parseInput(10000000000000)
+  print('machines:', len(machines))
+
+  ans = sum([t for t in map(findMinTokens, machines) if t is not None])
+  print(ans)
+
+part2()
