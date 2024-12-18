77c0d256bcae1a668750a6c7589bd20adda1b214
Eric Zamore <fzamore@gmail.com>
2024-12-17 21:42:15 -0500

    day17 part 2 done

--- 2024/day17.py
+++ 2024/day17.py
@@ -67,17 +67,77 @@ def execInstr(a: int, b: int, c: int, ip: int, instrs: list[int]) -> tuple[int, 
 
   return a, b, c, ip + 2, out
 
-def part1() -> None:
-  a, b, c, instrs = parseInput()
-  print('input:', a, b, c, instrs)
-
+def execProgram(a: int, b: int, c: int, instrs: list[int]) -> list[int]:
   outputs = []
   ip = 0
   while 0 <= ip < len(instrs):
     a, b, c, ip, out = execInstr(a, b, c, ip, instrs)
     if out is not None:
-      outputs.append(str(out))
+      outputs.append(out)
+  return outputs
 
-  print(','.join(outputs))
+# This function returns the value output by the program, given an input to
+# register A. This is based on manual analysis of my specific program
+# input.
+def computeOutput(a: int) -> int:
+  return (a ^ (a // 2 ** ((a % 8) ^ 7))) % 8
 
-part1()
+# This function starts with the rightmost (smallest) value, and builds up
+# until we have enough digits to satisfy the program.
+def findValue(level: int, value: int, program: list[int]) -> Optional[int]:
+  # Our target digit.
+  target = program[len(program) - (level + 1)]
+  assert 0 <= target < 8, 'bad target in program'
+
+  # Through manual analysis, I determined that the input program does this:
+  #
+  #   while A != 0:
+  #     B = mathematicalExpression(A)
+  #     output(B)
+  #     A = intdiv(A, 8)
+  #
+  # Thus, the program outputs exactly one octal digit per loop iteration.
+  # Since the program terminates when A == 0, A must be [1, 7] during the
+  # previous iteration of the loop. I derived how to compute B from A, so
+  # we test all values [1, 7] of A and for each of them that results in B,
+  # we have a candidate for that "level" of A. For each such candidate X,
+  # we test the next level by testing the values [8 * X, 8 * X + 7] (since
+  # that is the inverse of intdiv(8)). We continue this process until we
+  # have enough digits as are in the input program, and then we have our
+  # value of A.
+
+  # Since the program uses intdiv by 8, there are 8 possible values that
+  # could possibly result in the target.
+  for test in range(value, value + 8):
+    if computeOutput(test) == target:
+      print('match for level:', level, test)
+
+      if level == len(program) - 1:
+        # Huzzah! We have our final answer.
+        return test
+
+      # Each output cycle of the program does intdiv by 8, so we multiply
+      # by 8 and recur.
+      if (result := findValue(level + 1, test * 8, program)) is not None:
+        return result
+
+  # This branch was a dead-end
+  return None
+
+def part1() -> None:
+  a, b, c, instrs = parseInput()
+  print('input:', a, b, c, instrs)
+
+  outputs = execProgram(a, b, c, instrs)
+  print(','.join([str(o) for o in outputs]))
+
+def part2() -> None:
+  _, b, c, instrs = parseInput()
+  print('program:', instrs)
+
+  ans = findValue(0, 0, instrs)
+  assert ans is not None, 'did not find answer'
+  assert execProgram(ans, b, c, instrs) == instrs, 'found incorrect answer'
+  print(ans)
+
+part2()
