6a67252f98cf5b941afeba7d550fd7a582338002
Eric Zamore <fzamore@gmail.com>
2024-12-20 13:34:56 -0500

    day20 part 2 done

--- 2024/day20.py
+++ 2024/day20.py
@@ -19,19 +19,37 @@ def parseInput() -> tuple[ArrayGrid, Coords, Coords]:
   assert start is not None and end is not None, 'did not find start/end'
   return grid, start, end
 
-def tryCheats(grid: ArrayGrid, start: Coords, end: Coords, threshold: int = 100) -> int:
-  # Map of cheat coordinate to try (must be a wall) to the node that led us there.
-  cheatsToTry = {}
+def getCheatEnds(grid: ArrayGrid, cheatStart: Coords, cheatLength: int) -> Iterable[Coords]:
+  ends = set()
+  def visit(pos: Coords, numSteps: int) -> bool:
+    if numSteps > cheatLength:
+      return False
+
+    if pos != cheatStart:
+      ends.add(pos)
+    return True
+
+  bfs(cheatStart, lambda p: grid.getAdjacentCoords(p[0], p[1]), visit)
+  return ends
+
+def tryCheats(
+  grid: ArrayGrid,
+  start: Coords,
+  end: Coords,
+  cheatLength: int = 2,
+  threshold: int = 100,
+) -> int:
+  cheatsToTry = set()
   def getAdj(pos: Coords) -> Iterable[Coords]:
     x, y = pos
     for ax, ay, v in grid.getAdjacentItems(x, y):
       if v != '#':
         yield ax, ay
       else:
-        if (ax, ay) not in cheatsToTry:
+        if (x, y) not in cheatsToTry:
           # Always use the first value we encounter, as that is guaranteed
-          # to be the lowest in BFS order.
-          cheatsToTry[ax, ay] = pos
+          # to be the lowest distance in BFS order.
+          cheatsToTry.add((x, y))
 
   bfsResult = bfs(start, getAdj, isEndNode=lambda p: p == end)
 
@@ -42,26 +60,25 @@ def tryCheats(grid: ArrayGrid, start: Coords, end: Coords, threshold: int = 100)
   print()
 
   cheats = {}
-  for c1 in cheatsToTry:
-    c1x, c1y = c1
-    assert grid.getValue(c1x, c1y) == '#', 'all c1 should be walls: %s' % str(c1)
-    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-      c2x, c2y = c1x + dx, c1y + dy
-      c2 = c2x, c2y
+  for cheatStart in cheatsToTry:
+    c1x, c1y = cheatStart
+    assert grid.getValue(c1x, c1y) != '#', 'all c1 should be open'
+    for cheatEnd in getCheatEnds(grid, cheatStart, cheatLength):
+      c2x, c2y = cheatEnd
       if grid.getValue(c2x, c2y, '#') == '#':
         # The cheat would not land back on the track. Skip.
         continue
 
-      if c2 not in bfsResult:
+      if cheatEnd not in bfsResult:
         # If the cheat doesn't get us back on the shortest path, then skip.
         continue
 
-      prevNode = cheatsToTry[c1]
-
-      # The length of the new path is the sum of a/ the distance to the
-      # previous node, b/ 2 (for c1 & c2), and c/ the distance from c2 to
-      # the end.
-      pathLength = bfsResult[prevNode] + 2 + (shortestPathLength - bfsResult[c2])
+      # The length of the new path is the sum of a/ the distance the cheat
+      # start, b/ manhattan distance betwen cheat start and cheat end, and
+      # c/ the distance from cheat end to the course end.
+      cheatDist = abs(c1x - c2x) + abs(c1y - c2y)
+      assert cheatDist <= cheatLength, 'cheat was too long: %s %s %d' % (cheatStart, cheatEnd, cheatDist)
+      pathLength = bfsResult[cheatStart] + cheatDist + (shortestPathLength - bfsResult[cheatEnd])
       if pathLength >= shortestPathLength:
         # There is no savings on this path. Skip.
         continue
@@ -69,11 +86,11 @@ def tryCheats(grid: ArrayGrid, start: Coords, end: Coords, threshold: int = 100)
       savings = shortestPathLength - pathLength
       assert savings > 0, 'should have already checked for savings.'
 
-      assert (c1, c2) not in cheats, 'already seen cheat'
-      cheats[c1, c2] = savings
+      assert (cheatStart, cheatEnd) not in cheats, 'already seen cheat'
+      cheats[cheatStart, cheatEnd] = savings
 
 
-  # # Useful for debugging.
+  # Useful for debugging.
   # cheatsBySavings: dict[int, int] = Counter()
   # for c1, c2 in cheats:
   #   cheatsBySavings[cheats[c1, c2]] += 1
@@ -88,4 +105,10 @@ def part1() -> None:
 
   print(tryCheats(grid, start, end))
 
-part1()
+def part2() -> None:
+  grid, start, end = parseInput()
+  print('start/end:', start, end)
+
+  print(tryCheats(grid, start, end, 20))
+
+part2()
