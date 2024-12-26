2c52cabd44625a97e9261f5766a45b190560f14d
Eric Zamore <fzamore@gmail.com>
2024-12-25 21:17:04 -0500

    day24 part2 done. AoC 2024 done. thanks for playing

--- 2024/day24.py
+++ 2024/day24.py
@@ -19,8 +19,8 @@ Gates = dict[str, list[Gate]]
 
 Wires = dict[str, bool]
 
-def parseInput() -> tuple[Wires, Gates, BitWires]:
-  zwires: BitWires = {}
+def parseInput() -> tuple[Wires, Gates, list[str]]:
+  zwires = []
   gates = defaultdict(list)
   values = {}
   for line in input:
@@ -38,7 +38,7 @@ def parseInput() -> tuple[Wires, Gates, BitWires]:
       assert in1[0] != 'z' and in2[0] != 'z', 'input wires should not be z-wires'
       assert outwire[0] not in ['x', 'y'], 'output wires should not be x- or y-wires'
       if outwire[0] == 'z':
-        zwires[outwire] = None
+        zwires.append(outwire)
       gateType = [g for g in GateType if g.name == gateTypeStr][0]
       gate = Gate(gateType, in1, in2, outwire)
       gates[in1].append(gate)
@@ -69,7 +69,8 @@ def resolveGate(gate: Gate, values: dict[str, bool]) -> Optional[bool]:
     case _:
       assert False, 'bad gate type'
 
-def runMachine(values: dict[str, bool], gates: Gates, z: BitWires) -> int:
+def runMachine(values: dict[str, bool], gates: Gates, zwires: list[str]) -> int:
+  zbitwires: BitWires = dict((k, None) for k in zwires)
   while True:
     for wire in list(values.keys()):
       for gate in gates[wire]:
@@ -78,17 +79,109 @@ def runMachine(values: dict[str, bool], gates: Gates, z: BitWires) -> int:
 
         values[gate.out] = result
         if gate.out[0] == 'z':
-          z[gate.out] = result
+          zbitwires[gate.out] = result
 
-        if (zbitstring := getBitString(z)) is not None:
+        if (zbitstring := getBitString(zbitwires)) is not None:
           print('bitstring:', zbitstring)
           return int(zbitstring, 2)
 
-def part1() -> None:
-  values, gates, z = parseInput()
-  print('data:', len(values), len(gates), len(z))
+# Returns a gate matching the given gate type and input wire.
+def findGate(gates: Gates, type: GateType, inputWire: str) -> Optional[Gate]:
+  result = None
+  for gatelist in gates.values():
+    for gate in gatelist:
+      if gate.type == type and inputWire in [gate.in1, gate.in2]:
+        assert result is None or result == gate, 'duplicate gate found'
+        result = gate
 
-  ans = runMachine(values, gates, z)
+  return result
+
+def part1() -> None:
+  values, gates, zwires = parseInput()
+  print('data:', len(values), len(gates), len(zwires))
+
+  ans = runMachine(values, gates, zwires)
   print(ans)
 
-part1()
+def part2() -> None:
+  values, gates, zwires = parseInput()
+  print('data:', len(values), len(gates), len(zwires))
+
+  # Binary addition via AND, OR, and XOR gates is performed as follows.
+  # x_i and y_i are input bits, c is the carry bit (initially set to 0),
+  # and z_i is the output bit.
+  #
+  #   (x_i XOR y_i) XOR c => z_i
+  #   (x_i AND y_i) OR ((x_i XOR y_i) AND c) => c
+  #
+  # To reduce this to a "flat" list of gates where each input is only a
+  # single bit (as opposed to an expression), we introduce "temporary"
+  # bits/wires as p, q, and r:
+  #
+  #   x_i XOR y_i => p
+  #   x_i AND y_i => q
+  #   p AND c => r
+  #   c XOR p => z_i
+  #   q OR r => c
+  #
+  # This implies that for each bit, we need five gates. We look through
+  # our list of gates and find those that don't match this pattern
+  # (going bit by bit).
+
+  # Create a mapping from wire to gate whose output is that wire.
+  outgates = {}
+  for wire in gates:
+    for gate in gates[wire]:
+      outgates[gate.out] = gate
+
+  swaps = []
+  # We assume that wires for bits 00 and 45 are correctly formed (verified
+  # by experimentation).
+  for i in range(1, 45):
+    code = str(i).zfill(2)
+
+    xwire = 'x' + code
+    ywire = 'y' + code
+    zwire = 'z' + code
+
+    xyXorGate = findGate(gates, GateType.XOR, xwire)
+    assert xyXorGate is not None, 'did not find xy XOR gate'
+    assert ywire in [xyXorGate.in1, xyXorGate.in2], 'xy XOR gate did not have x and y as inputs'
+
+    xyAndGate = findGate(gates, GateType.AND, xwire)
+    assert xyAndGate is not None, 'did not find xy AND gate'
+    assert ywire in [xyAndGate.in1, xyAndGate.in2], 'xy AND gate did not have x and y as inputs'
+
+    # x_i XOR y_i => p
+    pwire = xyXorGate.out
+
+    # p AND c => r
+    rgate = findGate(gates, GateType.AND, pwire)
+    if rgate is None:
+      # There is no r gate whose type is AND.
+      qwire = xyAndGate.out
+
+      # q OR r => c
+      cgate = findGate(gates, GateType.OR, qwire)
+      assert cgate is None, 'c gate should also be messed up if r gate is messed up'
+
+      # Swap the pwire and qwire. This may not work in the general case,
+      # but it works for my input.
+      print('r gate problem:', i, pwire, qwire)
+      swaps.append((pwire, qwire))
+
+    # p XOR c => z_i
+    zgate = findGate(gates, GateType.XOR, pwire)
+    if zgate is not None and zgate != outgates[zwire]:
+      # The z gate's output is not correctly set (i.e., it's not to a
+      # z-wire). Make a swap so that the z-gate has an output of the
+      # z-wire.
+      assert zgate.out[0] != 'z', 'z gate should have had mismatch'
+      print('z gate mismatch:', i, zgate.out, outgates[zwire].out)
+      swaps.append((zgate.out, outgates[zwire].out))
+
+  print('swaps:', len(swaps), swaps)
+  ans = ','.join(sorted(sum(swaps, ())))
+  print(ans)
+
+part2()
