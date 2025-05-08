dcdbc1ad2714a46b835436931108ee0798a1855c
Eric Zamore <fzamore@gmail.com>
2025-05-08 13:36:47 -0400

    converting tabs to spaces

--- 2016/day21.py
+++ 2016/day21.py
@@ -6,96 +6,96 @@ input = open('day21.txt').read().splitlines()
 Seq = list[str]
 
 def swap(s: Seq, i: int, j: int) -> Seq:
-	# We don't need to do this in-place, but it's simple enough.
-	t = s[i]
-	s[i] = s[j]
-	s[j] = t
-	return s
+  # We don't need to do this in-place, but it's simple enough.
+  t = s[i]
+  s[i] = s[j]
+  s[j] = t
+  return s
 
 def rotate(s: Seq, n: int) -> Seq:
-	d = deque(s)
-	d.rotate(n)
-	return list(d)
+  d = deque(s)
+  d.rotate(n)
+  return list(d)
 
 def reverse(s: Seq, i: int, j: int) -> Seq:
-	assert i < j, 'bad reverse'
-	return s[:i] + s[i:j + 1][::-1] + s[j + 1:]
+  assert i < j, 'bad reverse'
+  return s[:i] + s[i:j + 1][::-1] + s[j + 1:]
 
 def move(s: Seq, i: int, j: int) -> Seq:
-	t = s.pop(i)
-	s.insert(j, t)
-	return s
+  t = s.pop(i)
+  s.insert(j, t)
+  return s
 
 def findReverseRotation(s: Seq, instr: str):
-	d = deque(s)
-	# We brute force all possible starting rotations to see if a regular
-	# rotate will yield the desired output. It's brute force, but it works
-	# because the length of the sequence is small.
-	for _ in range(len(d)):
-		d.rotate(1)
-		if executeInstr((p := list(d)), instr, rev=False) == s:
-			return p
-	assert False, 'did not find reverse rotation'
+  d = deque(s)
+  # We brute force all possible starting rotations to see if a regular
+  # rotate will yield the desired output. It's brute force, but it works
+  # because the length of the sequence is small.
+  for _ in range(len(d)):
+    d.rotate(1)
+    if executeInstr((p := list(d)), instr, rev=False) == s:
+      return p
+  assert False, 'did not find reverse rotation'
 
 def executeInstr(s: Seq, instr: str, *, rev: bool = False):
-	v = instr.split()
-	d = ints(instr)
-	match v[0]:
-		case 'swap':
-			match v[1]:
-				case 'position':
-					# Same forward and back.
-					return swap(s, d[0], d[1])
-				case 'letter':
-					i, j = (5, 2) if rev else (2, 5)
-					return swap(s, s.index(v[i]), s.index(v[j]))
-				case _:
-					assert False, 'bad swap instr'
-		case 'rotate':
-			match v[1]:
-				case 'left':
-					n = -d[0] if rev else d[0]
-					return rotate(s, -n)
-				case 'right':
-					n = -d[0] if rev else d[0]
-					return rotate(s, n)
-				case 'based':
-					if rev:
-						return findReverseRotation(s, instr)
-					n = s.index(v[-1])
-					n += 1 if n < 4 else 2
-					return rotate(s, n)
-				case _:
-					assert False, 'bad rotate instr'
-		case 'reverse':
-			# Same forward and back.
-			return reverse(s, d[0], d[1])
-		case 'move':
-			i, j = (d[1], d[0]) if rev else (d[0], d[1])
-			return move(s, i, j)
-		case _:
-			assert False, 'bad instr'
+  v = instr.split()
+  d = ints(instr)
+  match v[0]:
+    case 'swap':
+      match v[1]:
+        case 'position':
+          # Same forward and back.
+          return swap(s, d[0], d[1])
+        case 'letter':
+          i, j = (5, 2) if rev else (2, 5)
+          return swap(s, s.index(v[i]), s.index(v[j]))
+        case _:
+          assert False, 'bad swap instr'
+    case 'rotate':
+      match v[1]:
+        case 'left':
+          n = -d[0] if rev else d[0]
+          return rotate(s, -n)
+        case 'right':
+          n = -d[0] if rev else d[0]
+          return rotate(s, n)
+        case 'based':
+          if rev:
+            return findReverseRotation(s, instr)
+          n = s.index(v[-1])
+          n += 1 if n < 4 else 2
+          return rotate(s, n)
+        case _:
+          assert False, 'bad rotate instr'
+    case 'reverse':
+      # Same forward and back.
+      return reverse(s, d[0], d[1])
+    case 'move':
+      i, j = (d[1], d[0]) if rev else (d[0], d[1])
+      return move(s, i, j)
+    case _:
+      assert False, 'bad instr'
 
 def part1() -> None:
-	start = 'abcdefgh'
+  start = 'abcdefgh'
 
-	print('instrs:', len(input))
+  print('instrs:', len(input))
 
-	s = list(start)
-	print('input:', ''.join(s))
-	for instr in input:
-		s = executeInstr(s, instr)
-	print(''.join(s))
+  s = list(start)
+  print('input:', ''.join(s))
+  for instr in input:
+    s = executeInstr(s, instr)
+  print(''.join(s))
 
 def part2() -> None:
-	start = 'fbgdceah'
+  start = 'fbgdceah'
 
-	print('instrs:', len(input))
+  print('instrs:', len(input))
 
-	s = list(start)
-	print('input:', ''.join(s))
-	for instr in input[::-1]:
-		s = executeInstr(s, instr, rev=True)
-	print(''.join(s))
+  s = list(start)
+  print('input:', ''.join(s))
+  for instr in input[::-1]:
+    s = executeInstr(s, instr, rev=True)
+  print(''.join(s))
 
 part2()
--- 2016/day22.py
+++ 2016/day22.py
@@ -4,126 +4,126 @@ from itertools import combinations
 input = open('day22.txt').read().splitlines()[2:]
 
 def parseSizeSpec(s: str) -> int:
-	assert s[-1] == 'T', 'bad size spec'
-	return int(s[:-1])
+  assert s[-1] == 'T', 'bad size spec'
+  return int(s[:-1])
 
 def parseLine(grid: SparseGrid, line: str) -> None:
-	v = line.split()
-	d = [parseSizeSpec(v[i]) for i in [1, 2, 3]]
-	size, used, avail = d
-	assert size - used == avail, 'bad line'
+  v = line.split()
+  d = [parseSizeSpec(v[i]) for i in [1, 2, 3]]
+  size, used, avail = d
+  assert size - used == avail, 'bad line'
 
-	_, xs, ys = v[0].split('/')[-1].split('-')
-	assert xs[0] == 'x', 'bad x'
-	assert ys[0] == 'y', 'bad y'
+  _, xs, ys = v[0].split('/')[-1].split('-')
+  assert xs[0] == 'x', 'bad x'
+  assert ys[0] == 'y', 'bad y'
 
-	x, y = int(xs[1:]), int(ys[1:])
-	grid.setValue((x, y), (size, used))
+  x, y = int(xs[1:]), int(ys[1:])
+  grid.setValue((x, y), (size, used))
 
 def parseInput() -> SparseGrid:
-	grid = SparseGrid(2)
-	for line in input:
-		parseLine(grid, line)
-	return grid
+  grid = SparseGrid(2)
+  for line in input:
+    parseLine(grid, line)
+  return grid
 
 def isViablePair(grid: SparseGrid, c1: Coords, c2: Coords) -> bool:
-	assert c1 != c2, 'coords cannot be same'
-	_, u1 = grid.getValue(c1)
-	s2, u2 = grid.getValue(c2)
-	return 0 < u1 <= s2 - u2
+  assert c1 != c2, 'coords cannot be same'
+  _, u1 = grid.getValue(c1)
+  s2, u2 = grid.getValue(c2)
+  return 0 < u1 <= s2 - u2
 
 def moveData(grid: SparseGrid, src: Coords, dst: Coords) -> None:
-	ssize, sused = grid.getValue(src)
-	dsize, dused = grid.getValue(dst)
+  ssize, sused = grid.getValue(src)
+  dsize, dused = grid.getValue(dst)
 
-	assert dsize >= sused + dused, 'cannot move data'
+  assert dsize >= sused + dused, 'cannot move data'
 
-	grid.setValue(src, (ssize, 0))
-	grid.setValue(dst, (dsize, sused + dused))
+  grid.setValue(src, (ssize, 0))
+  grid.setValue(dst, (dsize, sused + dused))
 
 def part1() -> None:
-	print('nodes:', len(input))
+  print('nodes:', len(input))
 
-	grid = parseInput()
+  grid = parseInput()
 
-	ans = 0
-	for c1, c2 in combinations(grid.getAllCoords(), 2):
-		if isViablePair(grid, c1, c2):
-			ans += 1
-		if isViablePair(grid, c2, c1):
-			ans += 1
-	print(ans)
+  ans = 0
+  for c1, c2 in combinations(grid.getAllCoords(), 2):
+    if isViablePair(grid, c1, c2):
+      ans += 1
+    if isViablePair(grid, c2, c1):
+      ans += 1
+  print(ans)
 
 def part2() -> None:
-	print()
-	print('nodes:', len(input))
+  print()
+  print('nodes:', len(input))
 
-	grid = parseInput()
-	print('max coords:', grid.getMaxCoords())
+  grid = parseInput()
+  print('max coords:', grid.getMaxCoords())
 
-	maxX, _ = grid.getMaxCoords()
+  maxX, _ = grid.getMaxCoords()
 
-	for n in grid.getAllCoords():
-		if grid.getValue(n)[1] == 0:
-			zero = n
-	assert zero is not None, 'did not find zero node'
+  for n in grid.getAllCoords():
+    if grid.getValue(n)[1] == 0:
+      zero = n
+  assert zero is not None, 'did not find zero node'
 
-	print('zero node:', zero)
+  print('zero node:', zero)
 
-	# todo: add comment saying this solution is based on a horizontal wall
-	# thats anchored to the right of the grod above the zero node
+  # todo: add comment saying this solution is based on a horizontal wall
+  # thats anchored to the right of the grod above the zero node
 
   # This solution is based on the grid containing a horizontall "wall" of
   # nodes that cannot move their data (because they have too much). The
   # wall is anchored to the right of the grid, so the zero node must move
   # up and left around the wall.
 
-	steps = 0
-	x, y = zero
+  steps = 0
+  x, y = zero
 
-	# Moves the zero node.
-	def mz(dx, dy):
-		nonlocal x, y, steps
-		# moving the zero node is represented backward, since we're moving data into the zero node
-		src = x + dx, y + dy
-		dst = x, y
-		assert isViablePair(grid, src, dst), 'cannot move zero node'
-		moveData(grid, src, dst)
-		steps += 1
-		x += dx
-		y += dy
+  # Moves the zero node.
+  def mz(dx, dy):
+    nonlocal x, y, steps
+    # moving the zero node is represented backward, since we're moving data into the zero node
+    src = x + dx, y + dy
+    dst = x, y
+    assert isViablePair(grid, src, dst), 'cannot move zero node'
+    moveData(grid, src, dst)
+    steps += 1
+    x += dx
+    y += dy
 
-	# First, try to move the zero node up to the top, and if we encounter a
-	# wall, move it left around the wall.
-	while y > 0:
-		while not isViablePair(grid, (x, y - 1), (x, y)):
-			# Move left around the wall.
-			mz(-1, 0)
+  # First, try to move the zero node up to the top, and if we encounter a
+  # wall, move it left around the wall.
+  while y > 0:
+    while not isViablePair(grid, (x, y - 1), (x, y)):
+      # Move left around the wall.
+      mz(-1, 0)
 
-		# Move up.
-		mz(0, -1)
+    # Move up.
+    mz(0, -1)
 
-	print('first row:', x, y, steps)
+  print('first row:', x, y, steps)
 
-	# Next, move the zero node to the right until it's at the right edge.
-	while x < maxX:
-		mz(1, 0)
+  # Next, move the zero node to the right until it's at the right edge.
+  while x < maxX:
+    mz(1, 0)
 
-	print('at target:', x, y, steps)
+  print('at target:', x, y, steps)
 
-	# Now, the zero node has to move in a series of U-shaped patterns to the
-	# left untip we reach the source.
-	while (x, y) != (1, 0):
-		mz(0, 1)
-		mz(-1, 0)
-		mz(-1, 0)
-		mz(0, -1)
+  # Now, the zero node has to move in a series of U-shaped patterns to the
+  # left untip we reach the source.
+  while (x, y) != (1, 0):
+    mz(0, 1)
+    mz(-1, 0)
+    mz(-1, 0)
+    mz(0, -1)
 
-		# After we finish the U, we need to move the zero node right again (to
-		# re-position for the next U).
-		mz(1, 0)
+    # After we finish the U, we need to move the zero node right again (to
+    # re-position for the next U).
+    mz(1, 0)
 
-	print('done:', x, y, steps)
-	print(steps)
+  print('done:', x, y, steps)
+  print(steps)
 
 part2()
