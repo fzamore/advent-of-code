db16d7c7c60de623ce0b3f61f99bc6fe9a1222e9
Eric Zamore <fzamore@gmail.com>
2025-08-08 10:04:13 -0400

    removing pq from day19

--- 2015/day19_fromright.py
+++ 2015/day19_fromright.py
@@ -1,5 +1,4 @@
-from heapq import heappop, heappush
-from typing import Iterable
+from typing import Iterator
 
 data = open('day19.txt').read().splitlines()
 
@@ -25,7 +24,7 @@ def parse() -> tuple[Replacements, str]:
   assert start is not None, 'did not find start'
   return replacements, start
 
-def getReplacementsFromRight(replacements: Replacements, start: str) -> Iterable[str]:
+def getReplacementsFromRight(replacements: Replacements, start: str) -> Iterator[str]:
   for i in range(len(start), 0, -1):
     for src, dst in replacements:
       if start[i - len(src):i] == src:
@@ -41,24 +40,13 @@ def part2() -> None:
   print('target:', len(target), target)
 
   reverseMap = [(y, x) for (x, y) in replacements]
-  q: list[tuple[int, int, str]] = []
-  heappush(q, (len(target), 0, target))
-  while len(q) > 0:
-    length, steps, node = heappop(q)
-    assert length == len(node), 'bad queue management'
 
-    if node == 'e':
-      print('done.')
-      print(steps)
-      return
-
-    # Stolen from Reddit. If you always choose the first replacement when
-    # matching from the right, you'll never back yourself into a corner
-    # and will always be able to continue.
-    for rep in getReplacementsFromRight(reverseMap, node):
-      heappush(q, (len(rep), steps + 1, rep))
-      break
-
-  assert False, 'did not find answer'
+  molecule = target
+  steps = 0
+  while molecule != 'e':
+    molecule = next(getReplacementsFromRight(reverseMap, molecule))
+    steps += 1
+  print('done.')
+  print(steps)
 
 part2()
