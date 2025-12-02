5cc3537cb467461bc8c9e0e638fbc627f81b04b9
Eric Zamore <fzamore@gmail.com>
2025-12-02 11:23:06 -0500

    day1 cleanup

--- 2025/day1.py
+++ 2025/day1.py
@@ -5,7 +5,25 @@ data = open('day1.txt').read().splitlines()
 def parse() -> Iterable[int]:
   for line in data:
     r = -1 if line[0] == 'L' else 1
-    yield r * int(line[1:])
+    v = int(line[1:])
+    assert v != 0, 'no zeroes allowed'
+    yield r * v
+
+def doesPassZeroInPartialLoop(dial: int, v: int, m: int) -> bool:
+  newdial = (dial + v) % m
+  assert newdial != dial, 'no zeroes allowed'
+  if v > 0:
+    # Going right is simple; we pass zero in partial loop if we end up lower than we started.
+    return newdial < dial
+
+  if newdial == 0:
+    # We ended up at zero. Count it.
+    return True
+
+  # When going left, if we end up with a higher number than we started
+  # with, we passed zero in a partial loop. One exception to this is if
+  # we started at zero, would always trigger this condition.
+  return newdial > dial and dial != 0
 
 def part1() -> None:
   dial = 50
@@ -13,8 +31,7 @@ def part1() -> None:
   ans = 0
   for v in parse():
     dial = (dial + v) % m
-    if dial == 0:
-      ans += 1
+    ans += 1 if dial == 0 else 0
   print(ans)
 
 def part2() -> None:
@@ -24,19 +41,9 @@ def part2() -> None:
   for v in parse():
     # Full loops.
     ans += abs(v) // m
-
-    newdial = (dial + v) % m
-    if dial != 0:
-      # Special-case zero, and don't double-count it.
-      if newdial == 0:
-        ans += 1
-      # Partial loop going right.
-      elif v > 0 and newdial < dial:
-        ans += 1
-      # Partial loop going left.
-      elif v < 0 and newdial > dial:
-        ans += 1
-    dial = newdial
+    # Partial loop.
+    ans += 1 if doesPassZeroInPartialLoop(dial, v, m) else 0
+    dial = (dial + v) % m
   print(ans)
 
 part2()
