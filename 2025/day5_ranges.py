fcc31b6c0ada819cca2d39fac3734af53db49edd
Eric Zamore <fzamore@gmail.com>
2025-12-05 23:02:55 -0500

    add range-merging to library

--- 2025/day5_ranges.py
+++ 2025/day5_ranges.py
@@ -1,6 +1,6 @@
-data = open('day5.txt').read().splitlines()
+from common.ranges import Range, mergeRanges
 
-Range = tuple[int, int]
+data = open('day5.txt').read().splitlines()
 
 def parse() -> tuple[list[Range], list[int]]:
   ranges = []
@@ -16,34 +16,6 @@ def parse() -> tuple[list[Range], list[int]]:
       ingredients.append(int(line))
 
   return ranges, ingredients
-
-def isOverlapping(r1: Range, r2: Range) -> bool:
-  s1, e1 = r1
-  s2, e2 = r2
-  assert s1 <= e1 and s2 <= e2, 'bad range spec'
-  return not (e1 < s2 or e2 < s1)
-
-def mergeRange(ranges: list[Range], rng: Range) -> list[Range]:
-  newRanges = []
-  rangeToAdd = rng
-  for r in ranges:
-    if not isOverlapping(r, rangeToAdd):
-      # This range does not overlap. Maintain the existing range.
-      newRanges.append(r)
-      continue
-
-    start, end = rangeToAdd
-    s, e = r
-    rangeToAdd = (min(s, start), max(e, end))
-
-  newRanges.append(rangeToAdd)
-  return newRanges
-
-def mergeRanges(ranges: list[Range]) -> list[Range]:
-  mergedRanges: list[Range] = []
-  for r in ranges:
-    mergedRanges = mergeRange(mergedRanges, r)
-  return mergedRanges
 
 def part2() -> None:
   ranges, _ = parse()
--- common/ranges.py
+++ common/ranges.py
@@ -6,14 +6,53 @@ Range = tuple[int, int]
 def _validateRange(r: Range) -> None:
   assert type(r) is tuple and len(r) == 2 and r[0] <= r[1], 'bad range spec'
 
+# Returns whether the given ranges overlap.
+def isOverlapping(r1: Range, r2: Range) -> bool:
+  _validateRange(r1)
+  _validateRange(r2)
+  s1, e1 = r1
+  s2, e2 = r2
+  assert s1 <= e1 and s2 <= e2, 'bad range spec'
+  return not (e1 < s2 or e2 < s1)
+
 # Performs an intersection of the two ranges. None represents an empty
 # intersection.
 def intersectRanges(r1: Range, r2: Range) -> Optional[Range]:
   _validateRange(r1)
   _validateRange(r2)
-  if r2[0] > r1[1] or r1[0] > r2[1]:
+  if not isOverlapping(r1, r2):
     return None
   return (max(r1[0], r2[0]), min(r1[1], r2[1]))
+
+# Merges the given range into the list of ranges such that the given range
+# does not overlap with any existing ranges.
+def mergeRange(ranges: list[Range], rng: Range) -> list[Range]:
+  _validateRange(rng)
+  newRanges = []
+  rangeToAdd = rng
+  for r in ranges:
+    _validateRange(r)
+    if not isOverlapping(r, rangeToAdd):
+      # This range does not overlap. Maintain the existing range.
+      newRanges.append(r)
+      continue
+
+    # This range overlaps. Update our rangeToAdd
+    start, end = rangeToAdd
+    s, e = r
+    rangeToAdd = (min(s, start), max(e, end))
+
+  newRanges.append(rangeToAdd)
+  return newRanges
+
+# Takes a list of potentially overlapping ranges and returns a list of
+# equivalent ranges that are guaranteed to be disjoint.
+def mergeRanges(ranges: list[Range]) -> list[Range]:
+  mergedRanges: list[Range] = []
+  for r in ranges:
+    _validateRange(r)
+    mergedRanges = mergeRange(mergedRanges, r)
+  return mergedRanges
 
 # Subtracts r2 from r1. The result could be zero, one, or two ranges.
 SubtractionResult = tuple[Optional[Range], Optional[Range]]
@@ -61,16 +100,11 @@ def subtractMultipleRanges(subtrahend: Range, minuends: Iterable[Range]) -> list
   return results
 
 def _testSingle(r1: Range, r2: Range, expected: SubtractionResult) -> None:
-  print('Testing: "%s - %s"' % (r1, r2))
+  print('Testing: %s - %s' % (r1, r2))
   actual = subtractRange(r1, r2)
   assert actual == expected, 'test case failed. expected: %s: actual: %s' % (expected, actual)
 
-def _testMultiple(subtrahend: Range, minuends: Iterable[Range], expected: list[Range]) -> None:
-  print('Testing: "%s - %s"' % (subtrahend, minuends))
-  actual = subtractMultipleRanges(subtrahend, minuends)
-  assert actual == expected, 'test case failed. expected: %s: actual: %s' % (expected, actual)
-
-if __name__ == '__main__':
+def _testSubtractRange() -> None:
   singleTestcases: list[tuple[Range, Range, SubtractionResult]] = [
     ((1, 10), (-10, -1), (None, (1, 10))), # no overlap
     ((1, 10), (11, 15), ((1, 10), None)), # no overlap
@@ -81,11 +115,16 @@ if __name__ == '__main__':
     ((1, 10), (1, 10), (None, None)),
     ((1, 10), (10, 12), ((1, 9), None)),
   ]
-
-  print('testing subtract()')
+  print('testing subtractRange()')
   for r1, r2, expectedSingle in singleTestcases:
     _testSingle(r1, r2, expectedSingle)
 
+def _testMultiple(subtrahend: Range, minuends: Iterable[Range], expected: list[Range]) -> None:
+  print('Testing: %s - %s' % (subtrahend, minuends))
+  actual = subtractMultipleRanges(subtrahend, minuends)
+  assert actual == expected, 'test case failed. expected: %s: actual: %s' % (expected, actual)
+
+def _testSubtractMultipleRanges() -> None:
   multipleTestcases: list[tuple[Range, list[Range], list[Range]]] = [
     ((1, 10), [(2, 3), (2, 4), (6, 7)], [(1, 1), (5, 5), (8, 10)]),
     ((1, 10), [(-3, 3), (20, 40)], [(4, 10)]),
@@ -96,5 +135,44 @@ if __name__ == '__main__':
   print('testing subtractMultipleRanges()')
   for subtrahend, minuends, expectedMultiple in multipleTestcases:
     _testMultiple(subtrahend, minuends, expectedMultiple)
+
+def _testIsOverlapping() -> None:
+  print()
+  print('testing isOverlapping()')
+  isOverlappingTestCases: list[tuple[Range, Range, bool]] = [
+    ((1, 10), (20, 30), False),
+    ((1, 10), (10, 30), True),
+    ((1, 10), (2, 30), True),
+    ((1, 10), (2, 3), True),
+    ((1, 1), (2, 3), False),
+    ((1, 1), (2, 3), False),
+    ((1, 1), (1, 2), True),
+    ((20, 30), (1, 10), False),
+  ]
+  for r1, r2, expected in isOverlappingTestCases:
+    print('Testing: isOverlapping(%s, %s)' % (r1, r2))
+    actual = isOverlapping(r1, r2)
+    assert actual == expected, 'test failed. expected: %s; actual: %s' % (expected, actual)
+
+def _testMergeRanges() -> None:
+  print()
+  print('testing mergeRanges()')
+  mergeRangesTestCases: list[tuple[list[Range], list[Range]]] = [
+    ([(1, 3), (2, 2), (4, 7), (5, 10)], [(1, 3), (4, 10)]),
+    ([(2, 2), (4, 5), (1, 3), (1, 3), (2, 2), (4, 7), (5, 10)], [(1, 3), (4, 10)]),
+    ([(1, 3), (4, 7)], [(1, 3), (4, 7)]),
+    ([(1, 300), (2, 2), (4, 7), (5, 10)], [(1, 300)]),
+    ([(-10, -5), (-7, 2), (0, 0), (10, 20)], [(-10, 2), (10, 20)]),
+  ]
+  for ranges, expected in mergeRangesTestCases:
+    print('Testing: mergeRanges(%s)' % ranges)
+    actual = mergeRanges(ranges)
+    assert sorted(actual) == sorted(expected), 'test failed. expected: %s; actual: %s' % (expected, actual)
+
+if __name__ == '__main__':
+  _testSubtractRange()
+  _testSubtractMultipleRanges()
+  _testIsOverlapping()
+  _testMergeRanges()
 
   print('All tests passed.')
